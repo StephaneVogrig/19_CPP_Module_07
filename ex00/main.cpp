2f60c2c96d6cd607b88b3df2677bbdbdbfb09ee1
StephaneVogrig <stephane.vogrig@gmail.com>
2025-05-10 22:41:26 +0200

    ex00 ajout exercice complet

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,19 +6,31 @@
 /*   By: svogrig <svogrig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/23 21:27:07 by svogrig           #+#    #+#             */
-/*   Updated: 2025/03/23 21:49:30 by svogrig          ###   ########.fr       */
+/*   Updated: 2025/05/10 22:38:54 by svogrig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <cstdlib>
+#include "whatever.hpp"
 
-int main(int argc, char ** argv)
+int main(void)
 {
-	if (argc != 2)
-	{
-		std::cerr << "Usage: " << *argv << " <param>" << std::endl;
-		return (EXIT_FAILURE);
-	}
+	int a = 2;
+	int b = 3;
+
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	
 	return EXIT_SUCCESS;
 }
